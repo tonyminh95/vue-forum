215fcec10eb88019117311436e7c8f06dcd3d2a3
tonyminh95 <minhbui12121995@gmail.com>
2019-06-07 23:51:12 +0900

    declare a broadcast

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,48 @@
+<template>
+    <form @submit.prevent="addPost">
+        <div class="form-group">
+            <textarea
+                name=""
+                id=""
+                cols="30"
+                rows="10"
+                class="form-input"
+                v-model="newPostText"
+            ></textarea>
+        </div>
+        <div class="form-actions">
+            <button class="btn-blue">Submit post</button>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                newPostText: ''
+            }
+        },
+
+        methods: {
+            addPost () {
+				const postId = 'greatPost' + Math.random()
+				const post = {
+					text: this.newPostText,
+					publishedAt: Math.floor(Date.now() / 1000),
+					threadId: this.id,
+					userId: 'ALXhxjwgY9PinwNGHpfai6OWyDu2',
+					'.key': postId
+				}
+
+				// this.$set(sourceData.posts, postId, post)
+				// this.$set(this.thread.posts, postId, postId)
+				// this.$set(sourceData.users[post.userId].posts, postId, postId)
+
+                this.newPostText = ''
+
+                this.$emit('save-post', {post})
+			}
+        }
+    }
+</script>
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -3,21 +3,7 @@
 		<div class="col-large push-top">
 			<h1>{{ thread.title }}</h1>
 			<PostList :posts="posts"/>
-			<form @submit.prevent="addPost">
-				<div class="form-group">
-					<textarea
-						name=""
-						id=""
-						cols="30"
-						rows="10"
-						class="form-input"
-						v-model="newPostText"
-					></textarea>
-				</div>
-				<div class="form-actions">
-					<button class="btn-blue">Submit post</button>
-				</div>
-			</form>
+			<PostEditor/>
 		</div>
 	</div>
 </template>
@@ -25,10 +11,12 @@
 <script>
 	import sourceData from '@/data'
 	import PostList from '@/components/PostList'
+	import PostEditor from '@/components/PostEditor'
 
     export default {
 		components: {
-			PostList
+			PostList,
+			PostEditor
 		},
 
         props: {
@@ -40,8 +28,7 @@
 
         data () {
             return {
-				thread: sourceData.threads[this.id],
-				newPostText: ''
+				thread: sourceData.threads[this.id]
             }
 		},
 
@@ -55,22 +42,7 @@
 		},
 
 		methods: {
-			addPost () {
-				const postId = 'greatPost' + Math.random()
-				const post = {
-					text: this.newPostText,
-					publishedAt: Math.floor(Date.now() / 1000),
-					threadId: this.id,
-					userId: 'ALXhxjwgY9PinwNGHpfai6OWyDu2',
-					'.key': postId
-				}
 
-				this.$set(sourceData.posts, postId, post)
-				this.$set(this.thread.posts, postId, postId)
-				this.$set(sourceData.users[post.userId].posts, postId, postId)
-
-				this.newPostText = ''
-			}
 		}
     }
 </script>
