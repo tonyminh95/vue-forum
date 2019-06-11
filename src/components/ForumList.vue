56ba8c4a0a6c4bfacad02085e1d45b3819d4ece9
tonyminh95 <minhbui12121995@gmail.com>
2019-06-11 22:49:47 +0900

    Categories, Collections of Forums

--- src/components/ForumList.vue
+++ src/components/ForumList.vue
@@ -1,9 +1,5 @@
 <template>
-    <div class="forum-list">
-        <h2 class="list-title">
-            <a href="category.html">Forums</a>
-        </h2>
-
+    <div>
         <ForumListItem
             v-for="forum in forums"
             :forum="forum"
