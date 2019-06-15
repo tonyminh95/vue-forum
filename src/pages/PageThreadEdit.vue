ec34517a83357d80b6ef503869ddf4e775556abc
tonyminh95 <minhbui12121995@gmail.com>
2019-06-15 15:11:44 +0900

    Edit Threads

--- src/pages/PageThreadEdit.vue
+++ src/pages/PageThreadEdit.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="col-full push-top">
-        <h1>Create new thread in <i>{{ forum.name }}</i></h1>
+        <h1>Editing <i>{{ thread.title }}</i></h1>
 
         <ThreadEditor
+            :title="thread.title"
+            :text="text"
             @save="save"
             @cancel="cancel"
         />
@@ -18,22 +20,26 @@
         },
 
         props: {
-            forumId: {
+            id: {
                 type: String,
                 required: true
             }
         },
 
         computed: {
-            forum () {
-                return this.$store.state.forums[this.forumId]
+            thread () {
+                return this.$store.state.threads[this.id]
+            },
+
+            text () {
+                return this.$store.state.posts[this.thread.firstPostId].text
             }
         },
 
         methods: {
             save ({title, text}) {
-                this.$store.dispatch('createThread', {
-                    forumId: this.forum['.key'],
+                this.$store.dispatch('updateThread', {
+                    id: this.id,
                     title,
                     text
                 }).then(thread => {
