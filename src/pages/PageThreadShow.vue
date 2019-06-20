3a234932fb2db7e98c4255df7e99bbdc4fdf3bfa
tonyminh95 <minhbui12121995@gmail.com>
2019-06-20 23:05:43 +0900

    Start Migrate Vuex to use Firebase Realtime Database

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,33 +1,33 @@
 <template>
-    <div>
-		<div class="col-large push-top">
-			<h1>{{ thread.title }}
-				<router-link
-					:to="{name: 'ThreadEdit', id: this.id}"
-					class="btn-green btn-small"
-					tag="button"
-				>
-					Edit Thread
-				</router-link>
-			</h1>
+    <div v-if="thread && user" class="col-large push-top">
+		<h1>{{ thread.title }}
+			<router-link
+				:to="{name: 'ThreadEdit', id: this.id}"
+				class="btn-green btn-small"
+				tag="button"
+			>
+				Edit Thread
+			</router-link>
+		</h1>
 
-			<p>
-				By <a href="#" class="link-unstyled">{{ user.name }}</a>, <AppDate :timestamp="thread.publishedAt"/>.
-				<span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">{{repliesCount}} replies by {{contributorsCount}} contributors</span>
-			</p>
+		<p>
+			By <a href="#" class="link-unstyled">{{ user.name }}</a>, <AppDate :timestamp="thread.publishedAt"/>.
+			<span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">{{repliesCount}} replies by {{contributorsCount}} contributors</span>
+		</p>
 
-			<PostList :posts="posts"/>
+		<PostList :posts="posts"/>
 
-			<PostEditor
-				:threadId="id"
-			/>
-		</div>
+		<PostEditor
+			:threadId="id"
+		/>
 	</div>
 </template>
 
 <script>
+	import firebase from 'firebase'
 	import PostList from '@/components/PostList'
 	import PostEditor from '@/components/PostEditor'
+	import {countObjectProperties} from '@/utils'
 
     export default {
 		components: {
@@ -56,13 +56,7 @@
 			},
 
 			contributorsCount () {
-				const replies = Object.keys(this.thread.posts)
-					.filter(postId => postId !== this.thread.firstPostId)
-					.map(postId => this.$store.state.posts[postId])
-
-				const userIds = replies.map(post => post.userId)
-
-				return userIds.filter((item, index) => index === userIds.indexOf(item)).length
+				return countObjectProperties(this.thread.contributors)
             },
 
 			posts () {
@@ -71,6 +65,38 @@
 				return Object.values(this.$store.state.posts)
 					.filter(post => postIds.includes(post['.key']))
 			}
+		},
+
+		created () {
+			// fetch thread
+			firebase.database().ref('threads').child(this.id).once('value', snapshot => {
+				const thread = snapshot.val()
+
+				this.$store.commit('setThread', {threadId: snapshot.key, thread: {...thread, '.key': snapshot.key}})
+
+				// fetch user
+				firebase.database().ref('users').child(thread.userId).once('value', snapshot => {
+					const user = snapshot.val()
+
+					this.$store.commit('setUser', {userId: snapshot.key, user: {...user, '.key': snapshot.key}})
+				})
+
+				Object.keys(thread.posts).forEach(postId => {
+					// fetch post
+					firebase.database().ref('posts').child(postId).once('value', snapshot => {
+						const post = snapshot.val()
+
+						this.$store.commit('setPost', {postId: snapshot.key, post: {...post, '.key': snapshot.key}})
+
+						// fetch user
+						firebase.database().ref('users').child(post.userId).once('value', snapshot => {
+							const user = snapshot.val()
+
+							this.$store.commit('setUser', {userId: snapshot.key, user: {...user, '.key': snapshot.key}})
+						})
+					})
+				})
+			})
 		}
     }
 </script>
